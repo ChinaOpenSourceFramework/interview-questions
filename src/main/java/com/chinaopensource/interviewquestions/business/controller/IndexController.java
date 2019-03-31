a9ace12a114833bc85d8b279d05af3d2cae9298c
LiQiWei <liqiwei123@outlook.com>
2019-03-31 23:33:10 +0800

    添加docker

--- src/main/java/com/chinaopensource/interviewquestions/business/controller/IndexController.java
+++ src/main/java/com/chinaopensource/interviewquestions/business/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.chinaopensource.interviewquestions.business.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate ;
+	
 	@GetMapping("/")
 	public String getUser(Model model) {
 		model.addAttribute("user", "测试");
+		redisTemplate.opsForValue().set("liqiwei", "liqiwei");
 		return "index";
 	}
 }
